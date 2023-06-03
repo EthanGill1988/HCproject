4aec93109f6b029d7c38a9fcd0160ff2b2d7693d
Ethan Gill <1ethangill1@gmail.com>
2023-06-02 19:00:26 -0700

    Constructed drop down selector and designated vals

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var submitButtonEl = $('btn-primary');
-var productFormEl = $('productType')
+      let text = 'Value2';
+      $("select option").filter(function() {
+          //may want to use $.trim in here
+          return $(this).text() == text;
+        }).attr('selected', true);
+
+
+
+
+
+//var submitButtonEl = $('btn-primary');
+//var productFormEl = $('productType')
 
 //submitButtonEl.on('click', function() {
    // if ('#productType') {
