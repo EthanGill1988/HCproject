1163ccf4cda90eee97d216092e0d5e04f8da47ae
Ethan Gill <1ethangill1@gmail.com>
2023-06-03 20:09:20 -0700

    finished product type. started logging of input.

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-      let text = 'Value2';
+function choiceFunction() {
+    var x = document.getElementById('options').value;
+    document.getElementById('boxIt').innerHTML = x;
+    
+}
+
+function quantityFunction() {
+    var y = document.getElementById('quantity').value;
+    document.getElementById('boxIt2').innerHTML = y;
+    
+}
+
+function pointCalculator() {
+    var score = x.concat(y);
+}
+
+
+
+
+
+      let text = 'Value0';
       $("select option").filter(function() {
           //may want to use $.trim in here
           return $(this).text() == text;
